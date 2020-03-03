2aaaf62684aede0412fa434dc9aaca5873b05b6b
francfbs <francfbs@gmail.com>
2020-03-03 15:07:59 -0300

    refactor: valid password and password confirmation

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,14 +1,15 @@
+import { Controller } from './../interfaces/controller'
 import { badRequest } from './../helpers/http-helper'
 import { HttpResponse, HttpRequest } from './../interfaces/http'
 import { MissingParamError } from '../errors/missing-param-error'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
