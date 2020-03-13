117c2ce5f2b272443bc9a29d748971b71713d39f
fbsantos <fbsantos22@hotmail.com>
2020-03-12 22:15:48 -0300

    feat: bcrypt implementation and tests

---
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt'
+import { Encrypter } from '../../data/interfaces/encrypter'
+
+export class BcryptAdapter implements Encrypter {
+  private readonly salt: number
+  constructor (salt: number) {
+    this.salt = salt
+  }
+
+  async encrypt (value: string): Promise<string> {
+    return bcrypt.hash(value, 12)
+  }
+}
