54fc6ba4e4d92253f10573e3973e542f1a628fd4
fbsantos <fbsantos22@hotmail.com>
2020-03-26 17:23:31 -0300

    test: test to Validation Composite

---
+++ src/presentation/helpers/validations/validation-composite.spec.ts
@@ -0,0 +1,37 @@
+import { ValidationComposite } from './validation-composite'
+import { Validation } from './validation'
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationStubs: Validation[]
+}
+
+const makeSut = (): SutTypes => {
+  const validationStubs = makeValidation()
+  const sut = new ValidationComposite(validationStubs)
+  return { sut, validationStubs }
+}
+
+const makeValidation = (): Validation[] => {
+  class ValidationStub implements Validation {
+    validate (data: any): Error {
+      return null
+    }
+  }
+  return [new ValidationStub(), new ValidationStub()]
+}
+
+describe('ValidationComposite', () => {
+  test('Should return an error if any validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
+    const error = sut.validate({})
+    expect(error).toBeDefined()
+  })
+
+  test('Should return null if all validations pass', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({})
+    expect(error).toBeUndefined()
+  })
+})
