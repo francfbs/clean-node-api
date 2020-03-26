59d8f21b4af17b1b552079144e382de2bd67370f
fbsantos <fbsantos22@hotmail.com>
2020-03-26 16:56:29 -0300

    test: test to compare-field-validation

---
+++ src/presentation/helpers/validations/compare-field-validation.spec.ts
@@ -0,0 +1,20 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('field1', 'field2')
+}
+
+describe('RequiredField Validation', () => {
+  test('Should return a InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field1: 'fake value', field2: 'wrong value' })
+    expect(error).toEqual(new InvalidParamError('field2'))
+  })
+
+  test('Should return null if validation success', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field1: 'fake value', field2: 'fake value' })
+    expect(error).toBeUndefined()
+  })
+})
