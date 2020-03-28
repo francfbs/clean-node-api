da254a1466959c0f2cba4e586778c52e60aa1a8f
fbsantos <fbsantos22@hotmail.com>
2020-03-28 19:48:34 -0300

    feat: update access token repository implementation

--- src/infra/db/mongodb/account/account-reposiltory.spec.ts
+++ src/infra/db/mongodb/account/account-reposiltory.spec.ts
@@ -1,5 +1,15 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account-repository'
+import { Collection } from 'mongodb'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+
+let collection: Collection
+
+const makeFakeAddAccount = (): AddAccountModel => ({
+  name: 'any_name',
+  email: 'any_email@email.com',
+  password: 'any_password'
+})
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
@@ -7,7 +17,7 @@ describe('Account Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const collection = await MongoHelper.getCollection('accounts')
+    collection = await MongoHelper.getCollection('accounts')
     await collection.deleteMany({})
   })
 
@@ -19,17 +29,40 @@ describe('Account Mongo Repository', () => {
     return new AccountMongoRepository()
   }
 
-  test('Should return a account on success', async () => {
+  test('Should return a account on add success', async () => {
     const sut = makeSut()
-    const account = await sut.add({
-      name: 'any_name',
-      email: 'any_email@email.com',
-      password: 'any_password'
-    })
+    const account = await sut.add(makeFakeAddAccount())
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email@email.com')
     expect(account.password).toBe('any_password')
+  })
+
+  test('Should return a account on loadByEmail success', async () => {
+    const sut = makeSut()
+    await collection.insertOne(makeFakeAddAccount())
+    const account = await sut.loadByEmail('any_email@email.com')
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@email.com')
+    expect(account.password).toBe('any_password')
+  })
+
+  test('Should return null if loadByEmail cannot find register', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail('any_email@email.com')
+    expect(account).toBeNull()
+  })
+
+  test('Should update access token successfully', async () => {
+    const sut = makeSut()
+    const res = await collection.insertOne(makeFakeAddAccount())
+    expect(res.ops[0].token).toBeFalsy()
+    await sut.updateToken(res.ops[0]._id, 'any_token_updated')
+    const account = await collection.findOne({ _id: res.ops[0]._id })
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe('any_token_updated')
   })
 })
--- src/infra/db/mongodb/account/account-repository.ts
+++ src/infra/db/mongodb/account/account-repository.ts
@@ -1,11 +1,26 @@
 import { AddAccountRepository } from '../../../../data/interfaces/db/add-account-repository'
 import { AccountModel, AddAccountModel } from '../../../../data/usecases/add-account/db-add-account-interfaces'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { LoadAccountByEmailRepository } from '../../../../data/interfaces/db/load-account-by-email-repository'
+import { UpdateAccessTokenRepository } from '../../../../data/interfaces/db/update-access-token-repository'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
   async add (account: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(account)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+    return account && MongoHelper.map(account)
+  }
+
+  async updateToken (id: string, token: string): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne({ _id: id }, {
+      $set: { accessToken: token }
+    })
   }
 }
