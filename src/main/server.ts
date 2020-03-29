1bdd9b620c90bf237dfd14eb8130fedbfad0450a
fbsantos <fbsantos22@hotmail.com>
2020-03-29 15:37:16 -0300

    refactor: remove fg on route.config

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,6 +4,6 @@ import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 MongoHelper.connect(env.mongoUrl).then(async () => {
   const app = (await import('./config/app')).default
   app.listen(env.port, () => {
-    // console.log(`Server running on ${env.port} port!`)
+    console.log('Server running!')
   })
 }).catch(console.error)
