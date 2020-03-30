2f89d91f9e06c3eae395094987a6cd750422d00a
fbsantos <fbsantos22@hotmail.com>
2020-03-30 16:36:46 -0300

    refactor: mongo connection string remove

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://api:h50cOB9eXRkFL52K@cluster0-5fly8.azure.mongodb.net/test?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || '',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'e90e15bf35ac47819865169dcdb7b42b'
 }
